4a56d3a956a9f8030763f6f8cbc8c2b57a057375
nbky <nboukourray@gmail.com>
2020-01-02 00:51:52 +0100

    fixing iframe id

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ $(function() {
     ul.on("click", "li", function(e) {
         let id = $(this).attr('id');
         let url = `http://www.youtube.com/embed/${id}?enablejsapi=1&origin=http://example.com`;
-        $("#ytplayer").attr("src", url);
+        $("#player").attr("src", url);
 
     });
 
