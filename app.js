c07c8c52f2ae19e195024b7f2a761224598e4878
nbky <nboukourray@gmail.com>
2020-01-02 18:08:06 +0100

    styling the app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 $(function() {
 
-    let ul = $("#list");
+    let videoList = $("#videoList");
+    videoList.find("li").remove();
     search("mr bean");
 
     $("#searchForm").on("submit", function(event) {
         event.preventDefault();
-        ul.find("li").remove();
-        search($("#searchInput").val());
+        videoList.find("li").remove();
+        search($("#searchField").val());
 
     });
 
@@ -15,7 +16,7 @@ $(function() {
                 method: "GET",
                 url: "https://www.googleapis.com/youtube/v3/search",
                 data: {
-                    key: '',
+                    key: 'AIzaSyD-A54cg4_xQnDJCUtoY-ZWbm4DispeR58',
                     q: searchTerm,
                     part: 'snippet',
                     maxResults: 10,
@@ -26,21 +27,22 @@ $(function() {
             .done(function(data) {
                 let results = data.items;
                 let id = results[0].id.videoId
-                let url = `https://www.youtube.com/embed/${id}?enablejsapi=1&origin=http://example.com`;
+                let url = `https://www.youtube.com/embed/${id}?enablejsapi=1`;
                 $("#player").attr("src", url);
 
                 results.forEach((a) => {
-
-                    let li = `<li class="media my-3" id="${a.id.videoId}">
-                    <img class="mr-3" src="${a.snippet.thumbnails.medium.url}" alt="Generic placeholder image">
-                    <div class="media-body m-3">
-                        <h5 class="mt-0 mb-1">${a.snippet.title}</h5>
-                        <h6>${ new Intl.DateTimeFormat().format(new Date(a.snippet.publishedAt))}</h6>
-                        ${a.snippet.description}
-                    </div>
+                    let li = `<li class="list-group-item"><div class="media mb-3" id="${a.id.videoId}">
+                    <img class="align-self-center mr-2 d-none d-lg-block" src="${a.snippet.thumbnails.medium.url}" alt="">
+                    <img class="align-self-center mr-2 d-block d-lg-none" src="${a.snippet.thumbnails.default.url}" alt="">
+                    <div class="media-body" >
+                        <h6 class="mt-0">${a.snippet.title}</h6>
+                        <span>${ new Intl.DateTimeFormat().format(new Date(a.snippet.publishedAt))}</span>
+                        <div class="description">${a.snippet.description}</div>
+                    </div><div>
                 </li>`;
 
-                    ul.append(li);
+                    videoList.append(li);
+
                 });
             })
             .catch(function(msg) {
@@ -48,7 +50,7 @@ $(function() {
             });
     }
 
-    ul.on("click", "li", function(e) {
+    videoList.on("click", "li", function(e) {
         let id = $(this).attr('id');
         let url = `https://www.youtube.com/embed/${id}?enablejsapi=1&origin=http://example.com`;
         $("#player").attr("src", url);
